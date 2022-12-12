2f764de41bdb99948d42ab95a816153674892c0c
jitesh-k-runahr <jitesh.kusalkar@runahr.com>
2022-12-12 19:39:25 +0100

    refactor: reverting graphql setup

--- app/graphql/resolvers.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '../lib/prisma';
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-export const resolvers = {
-  Query: {
-    tenants: async () => await prisma.tenant.findMany(),
-  },
-};
